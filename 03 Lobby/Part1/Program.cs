9f6f62274aec86b3b5e9366a4ddae1d1e090de6b
Ulf Haueisen <ulf@dvlp.de>
2025-12-05 09:53:16 +0100

    Day 03

---
+++ 03 Lobby/Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        var sr = new StreamReader(@"..\..\input.txt");
+        int result = 0;
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            int fv = -1;
+            int fp = -1;
+
+            for (int i = 0; i < line.Length-1; i++)
+            {
+                if (line[i] - '0' > fv)
+                {
+                    fv = line[i] - '0';
+                    fp = i;
+                }
+            }
+
+            int sv = -1;
+            for (int i = fp+1; i < line.Length; i++)
+            {
+                if (line[i] - '0' > sv)
+                {
+                    sv = line[i] - '0';
+                }
+            }
+
+            Console.WriteLine($"{fv} {sv}");
+
+            result += fv * 10 + sv;
+
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ 03 Lobby/Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        var sr = new StreamReader(@"..\..\input.txt");
+        long result = 0;
+        while (!sr.EndOfStream)
+        {
+            long jolt = 0;
+            var line = sr.ReadLine();
+
+            int l = line.Length;
+            int r = 0;
+            for (int b = 12; b > 0; b--)
+            {
+                int m = -1;
+                for (int i = r; i < l - b + 1; i++)
+                {
+                    if (line[i] - '0' > m)
+                    {
+                        m = line[i] - '0';
+                        r = i + 1;
+                    }
+                }
+                jolt = jolt * 10 + m;
+            }
+
+            result += jolt;
+
+        }
+
+        Console.WriteLine(result);
+    }
+}

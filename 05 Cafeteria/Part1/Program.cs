3e739f6e96299d5587e7fed208c0515d4507e38a
Ulf Haueisen <ulf@dvlp.de>
2025-12-05 13:52:20 +0100

    Day 05

---
+++ 05 Cafeteria/Part1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        var sr = new StreamReader(@"..\..\input.txt");
+        int result = 0;
+        List<(long min, long max)> ranges = new();
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            if (line == "") break;
+            string[] p = line.Split('-');
+            ranges.Add((long.Parse(p[0]), long.Parse(p[1])));
+        }
+
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            long num = long.Parse(line);
+            foreach (var (min, max) in ranges)
+            {
+                if (num >= min && num <= max)
+                {
+                    result++;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
---
+++ 05 Cafeteria/Part2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        long result = 0;
+
+        List<(long min, long max)> ranges = new();
+        var sr = new StreamReader(@"..\..\input.txt");
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            if (line == "") break;
+            string[] p = line.Split('-');
+            ranges.Add((long.Parse(p[0]), long.Parse(p[1])));
+        }
+
+        ranges.Sort();
+
+        List<(long min, long max)> merged = new();
+        merged.Add(ranges[0]);
+
+        foreach (var r in ranges)
+        {
+            var last = merged[^1];
+            if (r.min <= last.max + 1)
+            {
+                merged[^1] = (last.min, Math.Max(last.max, r.max));
+            }
+            else
+            {
+                merged.Add(r);
+            }
+        }
+
+        foreach (var r in merged)
+        {
+            result += r.max-r.min+1;
+        }
+
+        Console.WriteLine(result);
+    }
+}

40fe6779c21d8653340cfd28eed262ce035c5f36
Ulf Haueisen <ulf@dvlp.de>
2025-12-08 11:59:16 +0100

    Day 07

---
+++ 07 Laboratories/Part1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        int result = 0;
+
+        HashSet<int> beams = new();
+        var sr = new StreamReader(@"..\..\input.txt");
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            if (beams.Count == 0)
+            {
+                beams.Add(line.IndexOf('S'));
+            }
+            else
+            {
+                HashSet<int> next = new();
+                foreach (int b in beams)
+                {
+                    if (line[b] == '^')
+                    {
+                        next.Add(b - 1);
+                        next.Add(b + 1);
+                        result++;
+                    }
+                    else
+                    {
+                        next.Add(b);
+                    }
+                }
+                beams = next;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ 07 Laboratories/Part2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AoC;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<int, long> beams = new();
+        var sr = new StreamReader(@"..\..\input.txt");
+        while (!sr.EndOfStream)
+        {
+            var line = sr.ReadLine();
+            if (beams.Count == 0)
+            {
+                beams.Add(line.IndexOf('S'), 1);
+            }
+            else
+            {
+                Dictionary<int, long> next = new();
+                foreach (var b in beams)
+                {
+                    if (line[b.Key] == '^')
+                    {
+                        if (!next.TryAdd(b.Key - 1, b.Value)) next[b.Key - 1] += b.Value;
+                        if (!next.TryAdd(b.Key + 1, b.Value)) next[b.Key + 1] += b.Value;
+                    }
+                    else
+                    {
+                        if (!next.TryAdd(b.Key, b.Value)) next[b.Key] += b.Value;
+                    }
+                }
+                beams = next;
+            }
+        }
+
+        long result = 0;
+        foreach (var b in beams)
+        {
+            result += b.Value;
+        }
+
+        Console.WriteLine(result);
+    }
+}
